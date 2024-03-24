d78a555c9ae89bc5095ec2c30ba7b8bea38a8d9c
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-03-24 10:41:54 +0100

    test config

--- src/config.rs
+++ src/config.rs
@@ -3,12 +3,23 @@ use std::fs;
 use std::io::{stdin, Read};
 
 pub fn config(filename: &str) -> Result<String, Box<dyn Error>> {
-    if filename == "-" {
-        let mut config = String::new();
-        stdin().read_to_string(&mut config)?; // TODO get from MainRunner
-        Ok(config)
-    } else {
-        let config = fs::read_to_string(filename)?;
-        Ok(config)
-    }
+	if filename == "-" {
+		let mut config = String::new();
+		stdin().read_to_string(&mut config)?; // TODO get from MainRunner
+		Ok(config)
+	} else {
+		let config = fs::read_to_string(filename)?;
+		Ok(config)
+	}
+}
+
+mod test {
+	#[test]
+	fn test_config() {
+		let config_result = super::config("stdin.yaml");
+		match config_result {
+			Ok(config) => assert_eq!(config, "key: value\n"),
+			Err(e) => panic!("Error: {}", e)
+		}
+	}
 }
